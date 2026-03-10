7ca6fe9cfb9fcc31d271ba7ddf01e48139a3f601
Monesh <hawkmone666@gmail.com>
2026-03-10 15:20:59 +0530

    admin.py

---
+++ admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Question, Choice, Submission
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+class LessonAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
