ff9209523c200baed5dc690ec03e848e33fa5fd7
Monesh <hawkmone666@gmail.com>
2026-03-10 16:21:25 +0530

    views.py

--- views.py
+++ views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Question, Choice, Submission
 
 
 def submit(request):
-    return HttpResponse("Exam submitted successfully")
+    return HttpResponse("Submission successful")
 
 
 def show_exam_result(request):
-    return HttpResponse("Exam result displayed")
+    return HttpResponse("Exam result")
