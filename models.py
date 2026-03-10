7c8a5ffe02316b7420d0a06ef23edc4085b0de53
Monesh <hawkmone666@gmail.com>
2026-03-10 16:33:11 +0530

    models.py

--- models.py
+++ models.py
@@ -1,7 +1,38 @@
 from django.db import models
 
+class Instructor(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Learner(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=200)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.question_text
@@ -18,4 +49,4 @@ class Choice(models.Model):
 
 class Submission(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    selected_choices = models.ManyToManyField(Choice)
